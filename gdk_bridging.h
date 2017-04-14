278defedd9c5e2c8e4fa412e7b477e595fe4b203
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-15 06:57:57 +1000

    Add #include <sys/types.h>

--- gdk_bridging.h
+++ gdk_bridging.h
@@ -79,6 +79,7 @@ struct _GdkMonitorClass {};
 struct _GdkMonitorInterface {};
 
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
+#include <sys/types.h>
 #include <unistd.h>
 #include <termios.h>
 #include <gdk/gdk.h>
